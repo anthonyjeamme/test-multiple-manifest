6580e322684957f02706aa0e870eee03e6d45b1a
anthonyjeamme <anthony@jeamme.fr>
2021-09-15 20:08:27 +0200

    add link to manifest

--- src/pages/lyon/index.tsx
+++ src/pages/lyon/index.tsx
@@ -1,6 +1,14 @@
 import React from "react"
+import Seo from "../../components/seo"
 
 const index = () => {
-  return <div className="index">Lyon</div>
+  return (
+    <div className="index">
+      <Seo>
+        <link rel="manifest" href="/lyon/manifest.webmanifest" />
+      </Seo>
+      Lyon
+    </div>
+  )
 }
 export default index
--- src/pages/paris/index.tsx
+++ src/pages/paris/index.tsx
@@ -1,6 +1,14 @@
 import React from "react"
+import Seo from "../../components/seo"
 
 const index = () => {
-  return <div className="index">Paris</div>
+  return (
+    <div className="index">
+      <Seo>
+        <link rel="manifest" href="/paris/manifest.webmanifest" />
+      </Seo>
+      Paris
+    </div>
+  )
 }
 export default index
