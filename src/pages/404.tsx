0e25f5a053429f43556bf79ca154e65a8f4472c2
anthonyjeamme <anthony@jeamme.fr>
2021-09-15 19:57:32 +0200

    first commit

---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+
+const NotFoundPage = () => <div>ok</div>
+
+export default NotFoundPage
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+
+const IndexPage = () => <div>Index</div>
+
+export default IndexPage
